6a92672bcefced492175ae11ffdd497076b0a118
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-04-06 18:13:30 +0100

    Add many many more test cases

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
-
 const tap = require('tap');
 const v3 = require('../src/v3');
+const fixture = require('./fixture.json');
 
-tap.equal(v3({ namespace: '', name: '5192250434001' }), '7da958ea-e5e6-3be1-87d0-8901c80a5e5d');
+fixture.hits.hits.forEach(({ _source }) => {
+	const brightcoveId = _source.webUrl.match(/\d{11,13}$/).pop();
+	tap.equal(v3({ namespace: '', name: brightcoveId }), _source.id);
+});
