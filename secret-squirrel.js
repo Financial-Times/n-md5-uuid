2a1eec86f1236d12b099c4bdad64f8dd93ceaede
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-08-15 13:48:37 +0100

    Add n-gage dependency, init secret squirrel  🐿 v2.5.16

---
+++ secret-squirrel.js
@@ -0,0 +1,112 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'564b8be3-0775-3531-b87b-f6da2272f642', // README.md:28, test/fixture.json:16|20
+			'38dbd827-fedc-3ebe-919f-e64cf55ea959', // README.md:31
+			'ee797a31-45d4-32d9-b869-2ed3d4331ab5', // test/fixture.json:26|30
+			'af914ab4-0131-3df8-8520-8f503e772b1c', // test/fixture.json:36|40
+			'a30d4395-4e94-3ac7-8c06-a3d2f211bad9', // test/fixture.json:46|50
+			'4f49bd74-a393-3928-8d92-24c885af8f7f', // test/fixture.json:56|60
+			'd8346186-ee50-31af-a5b0-526e49e97e8f', // test/fixture.json:66|70
+			'86cc9eaf-5e88-3dd2-b8d2-2fe3a83e28c4', // test/fixture.json:76|80
+			'48aa8e82-ea1b-37d5-a50d-2a38e3780e73', // test/fixture.json:86|90
+			'4af682b6-b703-3490-98cc-ea1cb4078960', // test/fixture.json:96|100
+			'05a8ccea-5437-32b8-b8c1-f821a6b554c9', // test/fixture.json:106|110
+			'c19ad5d9-322e-390c-8b21-ca622f647d5f', // test/fixture.json:116|120
+			'b02d5000-36e8-35ff-80f1-a5551d9c6b58', // test/fixture.json:126|130
+			'52930707-1a96-3eea-8dc4-550dea59ede4', // test/fixture.json:136|140
+			'74ac149d-5c45-3dcd-97f9-b625bf78decd', // test/fixture.json:146|150
+			'776d7f02-a604-385f-8825-74a3256e015c', // test/fixture.json:156|160
+			'3d15e638-ac0a-3d71-86ce-dcde471cfc55', // test/fixture.json:166|170
+			'0c8d156c-4965-3cf3-8159-912de9f315cb', // test/fixture.json:176|180
+			'3613297b-d742-3d23-a24f-6b7ad8e7988f', // test/fixture.json:186|190
+			'd1576a7b-bd26-3e2b-bbf6-263b2ae84fdb', // test/fixture.json:196|200
+			'32d9e641-3edd-364a-847a-dca09c6e97df', // test/fixture.json:206|210
+			'01721e94-027a-3beb-a95d-9932556eaf20', // test/fixture.json:216|220
+			'ab880ca2-2c58-3580-bc5d-94cbb0aa727b', // test/fixture.json:226|230
+			'dd35f6d9-0f9e-3427-8853-d399103d3ac3', // test/fixture.json:236|240, test/spec.js:15
+			'2669ca64-8486-3a9d-b8e0-332544effc85', // test/fixture.json:246|250
+			'49e764e7-c36f-338b-97e0-e39ebeb2d650', // test/fixture.json:256|260
+			'caadf072-d3a7-3885-88e3-6d627cb913f5', // test/fixture.json:266|270
+			'7ee68ff0-7590-385d-bee4-76cc38422c12', // test/fixture.json:276|280
+			'765b0451-1bb2-38ea-a5c3-7f6d68ebf023', // test/fixture.json:286|290
+			'b7270444-c2dd-3167-b824-3776c5ca0983', // test/fixture.json:296|300
+			'2c5d88fe-c365-300e-becb-38c3d0e94bcc', // test/fixture.json:306|310
+			'3e01708f-9702-3971-a48f-18472d46d015', // test/fixture.json:316|320
+			'ee0f931a-0125-3252-91fa-2a63b75a6f45', // test/fixture.json:326|330
+			'952fb64a-42b7-3c14-8693-2fb978661b11', // test/fixture.json:336|340
+			'5cf9bc92-62ed-3d26-82f9-1ccb9beaeeaa', // test/fixture.json:346|350
+			'6a2b7147-9554-338b-b44c-91a002efb046', // test/fixture.json:356|360
+			'34e73861-bbe2-383d-a4b8-090e7bf10d64', // test/fixture.json:366|370
+			'60b4aa7d-2ab6-3124-87fe-d082c162c079', // test/fixture.json:376|380
+			'3876d197-a4ab-3eb3-b4c2-9f2e123ccd7f', // test/fixture.json:386|390
+			'e47a43b9-b8d2-3658-a029-a8a15ff311c4', // test/fixture.json:396|400
+			'ba94ae86-d082-3152-92c1-e9f0414b605e', // test/fixture.json:406|410
+			'84e4aa55-2ffa-3e4c-b40f-c723146b2daa', // test/fixture.json:416|420
+			'3cabc635-60a1-3bfe-9ae5-2cf05b82d83a', // test/fixture.json:426|430
+			'c269a03a-9ce6-374b-9e4a-92f902c09860', // test/fixture.json:436|440
+			'510658a0-5bbf-3069-a6fb-daef1c51596b', // test/fixture.json:446|450
+			'edc37b49-6e7b-3876-aea5-501f01ef3017', // test/fixture.json:456|460
+			'cbaf9453-6ce0-38c3-9ae3-00b160507709', // test/fixture.json:466|470
+			'8d0efd46-d576-3477-af85-3a9437f9e219', // test/fixture.json:476|480
+			'da692984-5e9d-3eee-ba3c-911caa36b24d', // test/fixture.json:486|490
+			'8910b1a3-a387-337c-9794-31923697532c', // test/fixture.json:496|500
+			'4093a076-d963-3f71-ac52-dca11add9663', // test/fixture.json:506|510
+			'b3078550-080a-3e6d-b8ec-67b0383e42b6', // test/fixture.json:516|520
+			'4b37a26c-a07c-3b5a-8c94-b520ddc5aa2f', // test/fixture.json:526|530
+			'ac8778c5-9346-31f5-ada4-7d075eeb24b7', // test/fixture.json:536|540
+			'a51a7b55-fe32-3263-9d34-f8fa27142d74', // test/fixture.json:546|550
+			'2aa74fe0-5e73-3089-9fbe-fcca54a2e0d7', // test/fixture.json:556|560
+			'17a00044-792d-35a6-a5d2-ca6be8515bc9', // test/fixture.json:566|570
+			'09a378cf-2db3-36cc-a816-c71d684e5128', // test/fixture.json:576|580
+			'887448d1-0d4f-3fb0-b4bf-bcd92a23be6b', // test/fixture.json:586|590
+			'442b3680-5f80-3382-b5a2-0228b0b1cf97', // test/fixture.json:596|600
+			'726b857a-ef08-3244-859d-178763a8c879', // test/fixture.json:606|610
+			'06e9356c-708f-36e7-a744-f67a105f1458', // test/fixture.json:616|620
+			'726b813b-5327-39a3-8dcd-391702e816f0', // test/fixture.json:626|630
+			'21e6347d-dd04-3bf1-bc22-4cdc023264d0', // test/fixture.json:636|640
+			'e4d97ee9-505a-398d-83c1-85d4b4cd12b3', // test/fixture.json:646|650
+			'86010606-2d6b-3c95-bd18-5f788d002113', // test/fixture.json:656|660
+			'ccfdb029-8cf7-3968-b65d-35b8abfe9c7b', // test/fixture.json:666|670
+			'd48a5b1e-9989-384e-89f8-6be10703bacd', // test/fixture.json:676|680
+			'f2ad168d-8498-3b6a-80c7-ff021314a802', // test/fixture.json:686|690
+			'2b0f0bcc-6d6e-3d4a-bf6d-a9ed77efb959', // test/fixture.json:696|700
+			'bcbfb8ed-ac8b-3d42-8b54-03561e39b9f6', // test/fixture.json:706|710
+			'7af9a158-b9b5-36a4-9a6b-6db2abc26b56', // test/fixture.json:716|720
+			'4a064dfc-a918-3e53-8d59-a0cd89fb8c6f', // test/fixture.json:726|730
+			'f8f58ea8-a135-3f2f-846b-ae3a4c49c666', // test/fixture.json:736|740
+			'9bce4e2b-bdbc-3902-b193-93dee9b1446f', // test/fixture.json:746|750
+			'88e63c7e-7b33-36bb-91e0-b4eee4f44dda', // test/fixture.json:756|760
+			'69295ee1-eb46-3e54-a7ee-aa9769ed6346', // test/fixture.json:766|770
+			'556b263b-c643-3b1a-a00d-1389efc044f5', // test/fixture.json:776|780
+			'47839982-b373-3127-b13e-eefc6e18d93d', // test/fixture.json:786|790
+			'37e03559-774e-34f4-b720-09b46143f3e1', // test/fixture.json:796|800
+			'cff8f682-598c-3ceb-957a-a33deed77af3', // test/fixture.json:806|810
+			'094f77b6-8aa3-3d61-b960-45c62840d4da', // test/fixture.json:816|820
+			'1c33ea08-2478-3544-ae0a-0b8cb9712267', // test/fixture.json:826|830
+			'a2038847-36d4-363b-bc0c-f32d5594eb7f', // test/fixture.json:836|840
+			'8cd643a0-5f77-3d85-a0ce-6b4ed7a90809', // test/fixture.json:846|850
+			'6da19d9a-5d7b-3b65-9c0b-874f29133e94', // test/fixture.json:856|860
+			'3ec5e62c-0a38-3f8c-ae0a-cb0b3d8fd961', // test/fixture.json:866|870
+			'99b09a27-36e7-3988-9f84-c9515191cf9b', // test/fixture.json:876|880
+			'52f14aeb-3841-3961-81b0-43ace7fc34fd', // test/fixture.json:886|890
+			'978ecf40-93bb-3627-a0c9-043cf693479a', // test/fixture.json:896|900
+			'da91aace-fd64-3631-bb25-98b00a86a772', // test/fixture.json:906|910
+			'4152aee7-c36d-32a4-8152-6cde8e8eeeec', // test/fixture.json:916|920
+			'ddac5faa-5641-3bd9-a2bc-f94799238a24', // test/fixture.json:926|930
+			'2f1b509c-6312-3dc2-94bc-eb724dc18693', // test/fixture.json:936|940
+			'45189476-61da-3de3-9efb-b5f26886ec77', // test/fixture.json:946|950
+			'9cf4f2dd-84a2-3061-8643-56facd959978', // test/fixture.json:956|960
+			'5a0fae80-2bb6-3171-9c15-af927d28e4b0', // test/fixture.json:966|970
+			'145eb7e6-17d8-3080-828f-0921f11a6534', // test/fixture.json:976|980
+			'539557ff-3b5d-3260-afbe-41b0dcb1891c', // test/fixture.json:986|990
+			'05398e3b-8c3d-382f-8ef5-057b21177c7b', // test/fixture.json:996|1000
+			'8f21d3fd-ccf4-39e9-a359-e844cda82dbe' // test/fixture.json:1006|1010
+		]
+	}
+};
